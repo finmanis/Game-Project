ce4d95128437715f036aaddf332bd81ef6ba04de
finmanis <51968394+finmanis@users.noreply.github.com>
2019-10-20 22:32:52 -0400

    Changes
    Circle to rectangles,
    three rectangles,
    renamed as "dog"

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -1,39 +1,43 @@
 //create a variable to hold one ball
 let b;
 let anotherBall;
+let dog;
 
 function setup() {
   createCanvas(800, 400);
-  b = new Ball(0, 100,"red"); //make a new ball from the Ball class and call it b.
-  anotherBall = new Ball(200,20,"green");
+  b = new Dog(0, 100,"red"); //make a new ball from the Ball class and call it b.
+  anotherBall = new Dog(200,20,"green");
+  dog = new Dog(100,200,"yellow")
 }
 
 
 function draw(){
 	background(220);
-    b.drawBall(); //draw the ball called b (go look in the Ball class for the drawBall function)
-    b.moveBall(); //move the ball called b (go look in the Ball class for the moveBall function)
-    anotherBall.drawBall();
-    anotherBall.moveBall();
+    b.drawDog(); //draw the ball called b (go look in the Ball class for the drawBall function)
+    b.moveDog(); //move the ball called b (go look in the Ball class for the moveBall function)
+    anotherBall.drawDog();
+    anotherBall.moveDog();
+    dog.drawDog();
+    dog.moveDog();
 
 }
 
 
 //ball class from which to create new balls with similar properties.
-class Ball {
+class Dog {
 
 	constructor(x,y,color){ //every ball needs an x value and a y value
 		    this.x = x;
     		this.y = y;
-        this.color= color;
+        this.color = color;
 	}
-	drawBall(){  // draw a ball on the screen at x,y
-    		stroke(0);
+	drawDog(){  // draw a ball on the screen at x,y
+    		stroke(10);
     		fill(this.color);
-		    ellipse(this.x,this.y,10,10);
+		    rect(this.x,this.y,10,10);
 	}
-	moveBall(){ //update the location of the ball, so it moves across the screen
-		this.x = this.x+2;
-		this.y = this.y+.5;
+	moveDog(){ //update the location of the ball, so it moves across the screen
+		this.x = this.x+4;
+		this.y = this.y+2;
 	}
 }
